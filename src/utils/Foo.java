fe21964e4b9426418aef2fb9239f88c18f444fbe
Greggory Buehring <greggbuehring@gmail.com>
2021-08-25 21:15:08 -0400

    heyNow()

---
+++ src/utils/Foo.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'gregg1' branch, from Eclipse");
+		System.out.println("hello worlds");
+	}
+
+
+}
